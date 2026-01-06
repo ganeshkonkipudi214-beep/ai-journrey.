ea4bdad664c044435bfc9f59e0e5ba2a464fc4ee
ganeshkonkipudi214-beep <ganeshkonkipudi214@gmail.com>
2026-01-06 13:53:41 +0530

    Add files via upload

---
+++ day5_python.py
@@ -0,0 +1,12 @@
+marks = 20
+if marks >= 90:
+  print("Grade A (pass)")
+elif marks >=60:
+   print("Grade B (pass)")
+elif marks >=45:
+   print("Grade C (pass)")
+elif marks >= 35:
+   print("Grade D(pass)")
+else:
+    print("Fail")   
+        
